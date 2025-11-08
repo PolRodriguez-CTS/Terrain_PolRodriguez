b4300cbb0bf10f4b11c1ade6e7469ed33be45116
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2025-11-08 17:18:44 +0100

    luffy

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,9 +12,12 @@ public class PlayerController : MonoBehaviour
     public Vector2 _moveValue;
 
     //Camara
-    private Transform _mainCamera;
+    [SerializeField] private Transform _mainCamera;
     private float _turnSmoothVelocity;
     private float _smoothTime = 0.2f;
+
+    //Parámetros
+    private float _playerSpeed = 5;
 
     void Awake()
     {
@@ -42,11 +45,30 @@ public class PlayerController : MonoBehaviour
         _animator.SetFloat("Horizontal", _moveValue.x);
         _animator.SetFloat("Vertical", _moveValue.y);
 
-        if(direction != Vector3.zero)
+        if (direction != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTime);
             transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            _characterController.Move(moveDirection * _playerSpeed * Time.deltaTime);
         }
+    }
+
+    void Jump()
+    {
+        
+    }
+
+    void Gravity()
+    {
+        
+    }
+    
+    bool IsGrounded()
+    {
+        return true;
     }
 }
